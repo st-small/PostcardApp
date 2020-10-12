308091ffbb620b3f737805df7e2faf22f76dab1a
Станислав Шияновский <st-small@rambler.ru>
2020-10-12 20:29:02 +0300

    Initial Commit

---
+++ PostcardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostcardApp
+//
+//  Created by Stanly Shiyanovskiy on 12.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
